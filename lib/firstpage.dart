84bc3476dc8090bdbafabcb54bbced7723ac88df
mubashirmk <mubashirshamsudheen@gmail.com>
2024-11-30 13:31:04 +0530

    save 30/11/24

--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -14,101 +14,104 @@ class _FirstpageState extends State<Firstpage> {
   bool _showicons = false;
   bool _showfab = true;
 
-  Widget showIcons() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SizedBox(height: 50),
-        Container(
-          height: 45,
-          width: 45,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: Colors.green),
-          child: IconButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Taskscreen()));
-            },
-            icon: Icon(Icons.add_task),
-            color: Colors.black,
-          ),
-        ),
-        SizedBox(height: 20),
-        Container(
-          height: 45,
-          width: 45,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: Colors.green),
-          child: IconButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Listscreen()));
-            },
-            icon: Icon(Icons.check_box_rounded),
-            color: Colors.black,
-          ),
-        ),
-        SizedBox(height: 20),
-        Container(
-          height: 45,
-          width: 45,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: Colors.green),
-          child: IconButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Textscreen()));
-            },
-            icon: Icon(Icons.menu),
-            color: Colors.black,
-          ),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Echo Note",
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          bottom: TabBar(tabs: [
-            Text("Text"),
-            Text("List"),
-            Text("Task"),
-          ]),
-          backgroundColor: Colors.green,
-        ),
-        body: Center(
-          child: TabBarView(children: [
-            Textscreen(),
-            Listscreen(),
-            Taskscreen(),
-            if (_showicons)
-              Positioned(bottom: 80, right: 15, child: showIcons())
-          ]),
-        ),
-        floatingActionButton: _showfab
-            ? FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    _showicons = true;
-                    _showfab = false;
-                  });
-                },
-                child: Text(
-                  "+",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ))
-            : null,
-      ),
-    );
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "Echo Note",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              bottom: TabBar(tabs: [
+                Text("Text"),
+                Text("List"),
+                Text("Task"),
+              ]),
+              backgroundColor: Colors.green,
+            ),
+            floatingActionButton: _showfab
+                ? FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                        _showicons = true;
+                        _showfab = false;
+                      });
+                    },
+                    child: Text(
+                      "+",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ))
+                : null,
+            body: Stack(children: <Widget>[
+              if (_showicons)
+                Positioned(
+                  bottom: 80, // Adjust position above FAB
+                  right: 15,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(height: 50),
+                      Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green),
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Taskscreen()));
+                          },
+                          icon: Icon(Icons.add_task),
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green),
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Listscreen()));
+                          },
+                          icon: Icon(Icons.check_box_rounded),
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green),
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Textscreen()));
+                          },
+                          icon: Icon(Icons.menu),
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+            ])));
   }
 }
--- lib/taskscreen.dart
+++ lib/taskscreen.dart
@@ -6,10 +6,66 @@ class Taskscreen extends StatefulWidget {
 }
 
 class _TaskscreenState extends State<Taskscreen> {
+  final DateTime datetime = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("task screen")),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          "Add New Task",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.check,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 10),
+          TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text(
+                  "Title",
+                  style: TextStyle(color: Colors.green),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green))),
+          ),
+          SizedBox(height: 10),
+          SizedBox(
+            height: 100,
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text(
+                  "Description",
+                  style: TextStyle(color: Colors.green),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Text(
+                "${datetime.day.toString()}/${datetime.month.toString()}/${datetime.year.toString()}",
+                style: TextStyle(color: Colors.green),
+              ),
+              Spacer(),
+              Text(
+                "${datetime.hour.toString()}/${datetime.minute.toString()}",
+                style: TextStyle(color: Colors.green),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
